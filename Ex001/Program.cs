56ad6b1b0647383979059ec60667215e97d1f5a9
Olga_A <zagaes2@rambler.ru>
2022-10-17 10:38:07 +0300

    created program Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]}"+"; ");
+        }
+    Console.WriteLine();
+}
+
+void GenerateMassiv(int [] array, int min, int max)
+{
+    Random random = new Random();
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(min,max);
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[length];
+
+GenerateMassiv(array, 0, 2);
+
+PrintArray(array);
