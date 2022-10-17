fb6998c322c78040db7448b03a310761486da1bb
Olga_A <zagaes2@rambler.ru>
2022-10-17 11:54:53 +0300

    created program Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]}"+"; ");
+        }
+    Console.WriteLine();
+}
+
+void GenerateMassiv(int [] array, int min, int max)
+{
+    Random random = new Random();
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(min,max);
+    }
+}
+
+int SumElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (i%2!=0) sum=sum+array[i];
+        }
+    return sum;
+}
+
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальный элемент: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальный элемент: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[length];
+
+GenerateMassiv(array, min, max); // заполняем массив элементами
+
+PrintArray(array);
+
+int SumEl = SumElement(array); // считаем и возвращаем сумму чисел
+Console.WriteLine($"Сумма чисел на нечетной позиции : {SumEl}");
